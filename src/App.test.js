3a506de2c54fe252f1cdaccf6b3844ed36b78ce0
Carolina9503 <69082421+Carolina9503@users.noreply.github.com>
2022-02-19 18:21:33 -0500

    Feature(test) Add test for Home component

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,24 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { fireEvent, render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders title", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/My technical test in Fusion Talent/i);
   expect(linkElement).toBeInTheDocument();
+});
+test("renders table", () => {
+  render(<App />);
+  const tableElement = screen.getByRole("table");
+  expect(tableElement).toBeInTheDocument();
+});
+test("renders no found message", () => {
+  render(<App />);
+  const input = screen.getByLabelText("search-input");
+  fireEvent.change(input, { target: { value: "23222222" } });
+  const button = screen.getByLabelText("search-button");
+  fireEvent.click(button);
+  const notFoundMessage = screen.getByText(
+    /your search did not match any result/i
+  );
+  expect(notFoundMessage).toBeInTheDocument();
 });
